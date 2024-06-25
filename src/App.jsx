6c2de4faacebd1f037600d3c86eef72b6792201b
kris08052000 <krishnarajendrasagar7@gmail.com>
2024-06-25 20:40:40 +0530

    thrid

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Details from './pages/Details';
-import Add from './pages/Add';
-
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/Add' element={<Add/>} />
-        <Route path='/Details' element= {<Details/>}/>
-      </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
